b04b18146f59e16c781c2ba9ea27f4dc08884821
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2025-08-21 13:39:41 +0000

    del: old env implementation

--- lending_experiment/agents/ppo/ppo_wrapper_env.py
+++ lending_experiment/agents/ppo/ppo_wrapper_env.py
@@ -2,321 +2,86 @@ import gym
 import numpy as np
 import torch
 from gym import spaces
-from collections import deque
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-# remove sklearn convergence warning
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module='sklearn')
 
 from stable_baselines3.common.monitor import Monitor as SBMonitor
-
-
-def error_bound(X_0, X_1, error_source, model):
-    """Estimate the error on the rejected samples using the Proxy-A distance."""
-    if X_0.shape[0] == 0 or X_1.shape[0] == 0:
-        return 10
-    Y = np.concatenate([np.zeros(len(X_0)), np.ones(len(X_1))], axis=0)
-    X = np.concatenate([X_0, X_1], axis=0)
-    X_train, X_test, Y_train, Y_test = train_test_split(
-        X, Y, test_size=0.2, random_state=42, stratify=Y
-    )
-    model.fit(X_train, Y_train)
-    acc = accuracy_score(Y_test, model.predict(X_test))
-    if acc < 0.5:
-        Y_pred = 1 - model.predict(X_test)
-        acc = accuracy_score(Y_test, Y_pred)
-    err = 1 - acc
-
-    dist = 2 * (1 - 2 * err)
-    m = len(X_0)
-    d = X_0.shape[1]
-    delta = 0.05
-    c1 = 4 / m * np.sqrt(d * np.log(2 * np.exp(1) * m / d) + np.log(4 / delta))
-    c2 = 4 * np.sqrt( (d * np.log(2 * m) + np.log(4 / delta)) / m)
-
-    return error_source + dist #+ c1 + c2
 
 
 class Monitor(SBMonitor):
     def __init__(self, env, filename=None, allow_early_resets=True):
         super(Monitor, self).__init__(env, filename, allow_early_resets)
 
-    
     # ugly code to have the pred attribute
     @property
     def pred(self):
-        return getattr(self.env, 'pred', 0)
+        return getattr(self.env, "pred", 0)
 
     @pred.setter
     def pred(self, value):
         """
         Set the prediction value in the environment.
         """
-        if hasattr(self.env, 'pred'):
+        if hasattr(self.env, "pred"):
             self.env.pred = value
         else:
             raise AttributeError("Environment does not have 'pred' attribute")
-        
+
     @property
     def prob_accept(self):
-        return getattr(self.env, 'prob_accept', 1)
-    
+        return getattr(self.env, "prob_accept", 1)
+
     @prob_accept.setter
     def prob_accept(self, value):
         """
         Set the probability of acceptance in the environment.
         """
-        if hasattr(self.env, 'prob_accept'):
+        if hasattr(self.env, "prob_accept"):
             self.env.prob_accept = value
         else:
             raise AttributeError("Environment does not have 'prob_accept' attribute")
-    
+
     @property
     def prob_predict(self):
-        return getattr(self.env, 'prob_predict', 1)
+        return getattr(self.env, "prob_predict", 1)
 
     @prob_predict.setter
     def prob_predict(self, value):
         """
         Set the probability of prediction in the environment.
         """
-        if hasattr(self.env, 'prob_predict'):
+        if hasattr(self.env, "prob_predict"):
             self.env.prob_predict = value
         else:
             raise AttributeError("Environment does not have 'prob_predict' attribute")
 
 
-class PPOEnvWrapper_old(gym.Wrapper):
-    def __init__(self,
-        env,
-        reward_fn,
-        ep_timesteps=2000,
-        relaxed_timesteps=200,
-        mu_type = "qualification",
-        obs_type = "imputation",
-        zeta_0 = 1,
-        zeta_1 = 0,
-    ):
-        super(PPOEnvWrapper, self).__init__(env)
-        assert mu_type in ["qualification", "accuracy", "tpr"], f"mu_type {mu_type} not supported"
-        assert obs_type in ["imputation", "accepted", "full"], f"obs_type {obs_type} not supported"
-
-        self.observation_space = spaces.Box(
-            low=np.inf,
-            high=np.inf,
-            # (7) OHE of credit score + (2) group +    (2) mu of each group
-            shape=(env.observation_space['applicant_features'].shape[0] + 2 * env.state.params.num_groups,),
-        )
-
-        self.action_space = spaces.Discrete(n=2)
-
-        self.env = env
-        self.reward_fn = reward_fn()
-
-        self.timestep = 0
-        self.ep_timesteps = ep_timesteps
-        self.relaxed_timesteps = relaxed_timesteps
-        self.mu_type = mu_type
-        self.obs_type = obs_type
-        self.zeta_0 = zeta_0
-        self.zeta_1 = zeta_1
-
-        self.delta = np.zeros(1, )
-        self.old_bank_cash = 0
-        self.delta_delta = 0
-
-        # my addition
-        self.window = 1_000
-        self.y_real_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.y_pred_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.pred_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.prob_accept_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.a_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.x_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.pred = 0
-        self.prob_accept = 1
-        self.mu = np.zeros(2,)
-        self.mu_real = np.zeros(2,)
-        self.rejection = np.zeros(2,)
-        self.error_rejection = np.zeros(2,)
-        self.b_term = np.zeros(2,)
-
-    def process_observation(self, obs):
-        credit_score = obs['applicant_features']
-        group = obs['group']
-
-        return np.concatenate((credit_score, group, self.mu,), axis=0)
-
-    def reset(self):
-        self.timestep = 0
-        self.delta = np.zeros(1, )
-        self.old_bank_cash = 0
-        self.delta_delta = 0    
-        self.delta_b_term = 0
-
-
-        # my addiition
-        self.y_real_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.y_pred_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.pred_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.prob_accept_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.a_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.x_hist = [deque(maxlen=self.window) for _ in range(2)]
-        self.pred = 0
-        self.prob_accept = 1
-        self.prob_predict =1
-        self.mu = np.ones(2,)
-        self.mu_real = np.ones(2,)
-        self.rejection = np.zeros(2,)
-        self.error_rejection = np.zeros(2,)
-        self.b_term = np.zeros(2,)
-
-        self.discriminator = [LogisticRegression(
-            class_weight="balanced", 
-            solver="saga", 
-            warm_start=True, 
-            max_iter = 25,
-            tol=1e-3,
-        ) for _ in range(2)]
-
-        return self.process_observation(self.env.reset())
-    
-    def compute_mu(self):
-        for i in range(2):
-            y_real = np.array(self.y_real_hist[i])
-            y_pred = np.array(self.y_pred_hist[i])
-            pred = np.array(self.pred_hist[i])
-            prob_accept = np.array(self.prob_accept_hist[i])
-            action = np.array(self.a_hist[i])
-            x = np.array(self.x_hist[i])
-
-
-            # calculate first mu_real
-            if self.mu_type == "qualification":
-                self.mu_real[i] = y_real.mean() if len(y_real) > 0 else 1
-            elif self.mu_type == "accuracy":
-                self.mu_real[i] = (y_real == action).mean() if len(y_real) > 0 else 1
-            elif self.mu_type == "tpr":
-                self.mu_real[i] = np.mean(action[y_real == 1]) if y_real.sum() > 0 else 1
-            # now, calculate mu
-            if self.obs_type == "imputation":
-                if self.mu_type == "qualification":
-                    self.mu[i] = y_pred.mean() if len(y_pred) > 0 else 1
-                elif self.mu_type == "accuracy":
-                    self.mu[i] = (y_pred == action).mean() if len(y_pred) > 0 else 1
-                elif self.mu_type == "tpr":
-                    self.mu[i] = np.mean(action[y_pred == 1]) if y_pred.sum() > 0 else 1
-            elif self.obs_type == "accepted":
-                if self.mu_type == "qualification":
-                    self.mu[i] = y_pred[action == 1].mean() if (action == 1).sum() > 0 else 1
-                elif self.mu_type == "accuracy":
-                    self.mu[i] = (y_pred[action == 1] == 1).mean() if (action == 1).sum() > 0 else 1
-                elif self.mu_type == "tpr":
-                    self.mu[i] = 1
-            elif self.obs_type == "full":
-                self.mu[i] = self.mu_real[i]
-
-            # calculate rejection terms
-
-            self.b_term[i] = 0
-            continue
-        
-            self.rejection[i] = np.mean(action == 0) if len(action) > 0 else 0
-
-            error_source = np.abs(pred[action == 1] - y_real[action == 1]).mean() if action.sum() > 0 else 0
-            error_target = np.abs(y_pred[action == 0] - y_real[action == 0]).mean() if (action == 0).sum() > 0 else 0
-            x_0 = x[action == 0]
-            x_1 = x[action == 1]
-            if len(x) > 100:
-                error_target_bound = error_bound(x_0, x_1, error_source, self.discriminator[i])
-            else:
-                error_target_bound = 0
-
-            self.b_term[i] = error_target_bound * self.rejection[i]
-            #print(f"Group {i}, r(s) : {self.rejection[i]:.2f} e(s) : {error_source:.2f} e(t) : {error_target:.2f} e(t) bound : {error_target_bound:.2f}")
-
-        #print("")
-            
-
-
-    def step(self, action):
-        old_delta = self.delta
-
-        # Update instance variables before we step the environment
-        group_id = np.argmax(self.env.state.group)
-        self.old_bank_cash = self.env.state.bank_cash
-        label = 1 - self.env.state.will_default
-
-        if self.obs_type == "imputation":
-            pred = self.pred if action == 0 else label
-        else:
-            pred = label
-
-        self.prob_accept = self.prob_accept + 1e-6
-        self.y_real_hist[group_id].append(label)
-        self.y_pred_hist[group_id].append(pred)
-        self.pred_hist[group_id].append(self.pred)
-        self.prob_accept_hist[group_id].append(self.prob_accept)
-        self.a_hist[group_id].append(action)
-        self.x_hist[group_id].append(self.env.state.applicant_features)
-        self.compute_mu()
-
-        self.delta = np.abs(self.mu[0] - self.mu[1])
-        self.delta_real = np.abs(self.mu_real[0] - self.mu_real[1])
-        self.delta_delta = self.delta - old_delta
-        self.delta_b_term = np.abs(self.b_term[0] - self.b_term[1])
-
-        if self.timestep < self.relaxed_timesteps:
-            ratio = self.timestep / self.relaxed_timesteps
-            self.delta *= ratio
-            self.delta_real *= ratio
-
-        obs, _, done, info = self.env.step(action)
-
-        r = self.reward_fn(
-            old_bank_cash=self.old_bank_cash,
-            bank_cash=self.env.state.bank_cash,
-            tpr=self.mu,
-            zeta0=self.zeta_0,
-            zeta1=self.zeta_1
-        )
-
-        self.timestep += 1
-        if self.timestep >= self.ep_timesteps:
-            done = True
-
-        return self.process_observation(obs), r, done, info
-
-
 class PPOEnvWrapper(gym.Wrapper):
-    def __init__(self,
+    def __init__(
+        self,
         env,
-        ep_timesteps=2000,
     ):
         super(PPOEnvWrapper, self).__init__(env)
 
         self.observation_space = spaces.Box(
             low=np.inf,
             high=np.inf,
-            shape=(env.observation_space['applicant_features'].shape[0] + env.state.params.num_groups,),
+            shape=(
+                env.observation_space["applicant_features"].shape[0]
+                + env.state.params.num_groups,
+            ),
         )
 
         self.action_space = spaces.Discrete(n=2)
         self.env = env
-        self.timestep = 0
-        self.ep_timesteps = ep_timesteps
         self.num_applicants = len(self.env.pool)
         self.env.predict_fn = self._predict_fn
 
     def set_agent(self, agent):
         self.policy = agent.policy
-        
+
     def _predict_fn(self, applicant):
         obs = {
-            "applicant_features": applicant['features'],
-            "group": applicant['group'],
+            "applicant_features": applicant["features"],
+            "group": applicant["group"],
         }
         obs = self.process_observation(obs)
         obs = np.array(obs).reshape(1, -1)
@@ -324,31 +89,26 @@ class PPOEnvWrapper(gym.Wrapper):
         with torch.no_grad():
             pred = self.policy.predict_label(obs).cpu().numpy()[0]
         return pred
-        
+
     def get_applicant_obs(self, idx):
         """Get the observation for a specific applicant."""
         applicant = self.env.pool[idx]
         obs = {
-            'applicant_features': applicant['features'],
-            'group': applicant['group'],
+            "applicant_features": applicant["features"],
+            "group": applicant["group"],
         }
         return self.process_observation(obs)
 
     def process_observation(self, obs):
-        credit_score = obs['applicant_features']
-        group = obs['group']
+        credit_score = obs["applicant_features"]
+        group = obs["group"]
         return np.concatenate((credit_score, group), axis=0)
 
     def reset(self):
-        self.timestep = 0
         return self.process_observation(self.env.reset())
-    
+
     def step(self, action):
         old_resource = self.env.state.resource
         obs, _, done, info = self.env.step(action)
         r = self.env.state.resource - old_resource
-        self.timestep += 1
-        if self.timestep >= self.ep_timesteps:
-            done = True
-
         return self.process_observation(obs), r, done, info
